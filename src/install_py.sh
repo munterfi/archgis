199b7200a77564bc2a1511f8e614cb0e4a854697
Merlin Unterfinger <info@munterfinger.ch>
2020-02-04 17:40:18 +0100

    Change spatial python env location

--- src/install_py.sh
+++ src/install_py.sh
@@ -3,10 +3,14 @@
 # Install python virtual envs
 pacman -S --noconfirm python-virtualenv
 
-# Create new env "spatial"
+# Create new env "spatial" in /home/<usr>/py
+mkdir "/home/$SUDO_USER/py" && cd "$_"
 virtualenv -p /usr/bin/python3 spatial
-source spatial/bin/activate
+source spatial/bin/activate && cd -
 
 # Install spatial packages
-pip install geopandas rasterio pydeck ipykernel
-ipython kernel install --user #--project=spatial
+pip install geopandas rasterio pydeck
+
+# Make env availale to jupyter lab
+pip install ipykernel
+python -m ipykernel install --name=spatial
