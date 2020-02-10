c17c4867e84e132297149a3ef820adf169169094
Merlin Unterfinger <info@munterfinger.ch>
2020-02-10 08:10:10 +0100

    Add ArchGDAL to julia package installation

--- src/install_julia.sh
+++ src/install_julia.sh
@@ -4,25 +4,13 @@
 pacman -S --noconfirm julia
 
 # Install packages
-julia -e 'using(Pkg); Pkg.add("DataFrames");'
-julia -e 'using(Pkg); Pkg.add("Distributions");'
-julia -e 'using(Pkg); Pkg.add("RDatasets");'
-julia -e 'using(Pkg); Pkg.add("Plots");'
-julia -e 'using(Pkg); Pkg.add("StatsPlots");'
+julia -e 'import Pkg; Pkg.add("DataFrames");'
+julia -e 'import Pkg; Pkg.add("ArchGDAL");'
+julia -e 'import Pkg; Pkg.add("Distributions");'
+julia -e 'import Pkg; Pkg.add("RDatasets");'
+julia -e 'import Pkg; Pkg.add("Plots");'
+julia -e 'import Pkg; Pkg.add("StatsPlots");'
 
 # Make available to jupyter lab
-julia -e 'using(Pkg); Pkg.add("IJulia");'
-
-# Create named pipe
-#mkfifo pipe
-
-# Keep pipe alive
-#sleep 1000 > pipe &
-
-# Make Julia read from pipe
-#julia < pipe &
-#echo 'using(Pkg)' > pipe
-#echo 'Pkg.add("DataFrames")' > pipe 
-#echo 'exit()' > pipe
-#echo 'while true end' > pipe
+julia -e 'import Pkg; Pkg.add("IJulia");'
 
