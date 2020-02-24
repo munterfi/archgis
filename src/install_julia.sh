202cfa6db38f49ee1887eaab100714c6e8e91612
Merlin Unterfinger <info@munterfinger.ch>
2020-02-24 08:42:46 +0100

    Change installation of julia from pacman to binary

--- src/install_julia.sh
+++ src/install_julia.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-# Install Julia
-pacman -S --noconfirm julia
+# Avoid installing julia via pacman
+# pacman -S --noconfirm julia"
+# ... as it causes serious issues while installing julia packages.
+
+# Install binary directly from https://julialang.org/downloads/
+vers=julia-1.3.1
+cd $ARCHGIS_PATH
+curl https://julialang-s3.julialang.org/bin/linux/x64/1.3/$vers-linux-x86_64.tar.gz -so julia.tar.gz
+tar -xzf julia.tar.gz && rm -rf julia.tar.gz && mv $vers julia && cd -
+
+# Export PATH
+echo 'export PATH=$PATH:'$ARCHGIS_PATH'/julia/bin' >> $ARCHGIS_PROFILE
+. $ARCHGIS_PROFILE
 
 # Install packages
 julia -e 'import Pkg; Pkg.add("DataFrames");'
@@ -13,6 +24,6 @@ julia -e 'import Pkg; Pkg.add("Distributions");'
 julia -e 'import Pkg; Pkg.add("Plots");'
 julia -e 'import Pkg; Pkg.add("StatsPlots");'
 
-# Make available to jupyter lab
+# Make julia available to jupyter lab
 julia -e 'import Pkg; Pkg.add("IJulia");'
 
